ec3c27ab045f12534267c02f81ff9a259c8ee2fd
boranblok <ben.blok@gmail.com>
2015-04-20 21:27:42 +0200

    The tool now generates an NZB file. But this logic should be moved to a more dedicated location.

--- nntpPoster/PostedFileSegment.cs
+++ nntpPoster/PostedFileSegment.cs
@@ -10,5 +10,12 @@ namespace nntpPoster
         public Int64 Bytes { get; set; }
         public Int32 SegmentNumber { get; set; }
         public String MessageId { get; set; }
+        public String MessageIdWithoutBrackets 
+        { 
+            get
+            {
+                return MessageId.TrimStart('<').TrimEnd('>');
+            }
+        }
     }
 }
--- nntpPoster/Program.cs
+++ nntpPoster/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using NntpClientLib;
 
 namespace nntpPoster
@@ -16,6 +17,8 @@ namespace nntpPoster
             FileToPost toPost = new FileToPost(new FileInfo("testFiles\\BG2sizeComp.jpg"));
             PostedFileInfo postInfo = toPost.PostYEncFile(poster, "1/1", "");
             poster.WaitTillCompletion();
+            XDocument nzbDoc = GenerateNzbFromPostInfo(toPost.FileName, new List<PostedFileInfo>(new PostedFileInfo[] { postInfo }));
+            nzbDoc.Save(toPost.FileName + ".nzb");
             return;
 
             //using (Rfc977NntpClientWithExtensions client = new Rfc977NntpClientWithExtensions())
@@ -38,6 +41,45 @@ namespace nntpPoster
 
             //    client.PostArticle(new ArticleHeadersDictionaryEnumerator(headers), body);
             //}
+        }
+
+        private static XDocument GenerateNzbFromPostInfo(String title, List<PostedFileInfo> postedFiles)
+        {
+            XNamespace ns = "http://www.newzbin.com/DTD/2003/nzb";
+
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XDocumentType("nzb", "-//newzBin//DTD NZB 1.1//EN", "http://www.newzbin.com/DTD/nzb/nzb-1.1.dtd", null),
+                new XElement(ns + "nzb",
+                    new XElement(ns + "head",
+                        new XElement(ns + "meta",
+                            new XAttribute("type", "title"),
+                            title
+                        )
+                    ),
+                    postedFiles.Select(f =>
+                        new XElement(ns + "file",
+                            new XAttribute("poster", PostSettings.FromAddress),
+                            new XAttribute("date", f.PostedDateTime.GetUnixTimeStamp()),
+                            new XAttribute("subject", f.NzbSubjectName),
+                            new XElement(ns + "groups",
+                                f.PostedGroups.Select(g => new XElement(ns + "group", g))
+                            ),
+                            new XElement(ns + "segments",
+                                f.Segments.OrderBy(s => s.SegmentNumber).Select(s =>
+                                    new XElement(ns + "segment",
+                                        new XAttribute("bytes", s.Bytes),
+                                        new XAttribute("number", s.SegmentNumber),
+                                        s.MessageIdWithoutBrackets
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+
+            return doc;
         }
     }
 }
