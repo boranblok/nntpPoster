8cf050c972656da39e5e8b07eacd6eebc706b3f4
boranblok <ben.blok@gmail.com>
2015-04-27 13:56:04 +0200

    The watching and moving of files has moved to a seperate class, maing the program only a template function.

--- nntpAutoposter/Program.cs
+++ nntpAutoposter/Program.cs
@@ -13,38 +13,34 @@ namespace nntpAutoposter
 {
     class Program
     {
-        static AutoPosterConfig autoPosterconfig;
+        static AutoPosterConfig configuration;
         static void Main(string[] args)
         {
-            autoPosterconfig = new AutoPosterConfig();
+            configuration = new AutoPosterConfig();
 
-            foreach (FileSystemInfo toPost in autoPosterconfig.WatchFolder.EnumerateFileSystemInfos())
-            {
-                MoveToBackupFolderAndPost(toPost.FullName);
-            }
-            FileSystemWatcher watcher = new FileSystemWatcher(autoPosterconfig.WatchFolder.FullName);
-            watcher.Created += watcher_Created;
-            watcher.Error += watcher_Error;
-            watcher.IncludeSubdirectories = false;
-            watcher.EnableRaisingEvents = true;
+            Watcher watcher = new Watcher(configuration);
+            watcher.Start();
+            Console.WriteLine("FileSystemWatcher started");
 
-            Console.WriteLine("Monitoring '{0}' for new files or folders to post.",
-                autoPosterconfig.WatchFolder.FullName);
-
-            AutoPoster poster = new AutoPoster(autoPosterconfig);
+            AutoPoster poster = new AutoPoster(configuration);
             poster.Start();
             Console.WriteLine("Autoposter started");
 
-            IndexerNotifier notifier = new IndexerNotifier(autoPosterconfig);
+            IndexerNotifier notifier = new IndexerNotifier(configuration);
             notifier.Start();
             Console.WriteLine("Notifier started");
 
-            IndexerVerifier verifier = new IndexerVerifier(autoPosterconfig);
+            IndexerVerifier verifier = new IndexerVerifier(configuration);
             verifier.Start();
             Console.WriteLine("Verifier started");
 
+
             Console.WriteLine("Press any key to stop after the current operations have finished.");
             Console.ReadKey();
+
+
+            watcher.Stop();
+            Console.WriteLine("FileSystemWatcher stopped");
 
             verifier.Stop();
             Console.WriteLine("Verifier stopped");
@@ -54,99 +50,6 @@ namespace nntpAutoposter
 
             poster.Stop();
             Console.WriteLine("Autoposter stopped");
-        }
-
-        static void watcher_Error(object sender, ErrorEventArgs e)
-        {
-            Console.WriteLine("The FileSystemWatcher got an error:");
-            Console.WriteLine(e.GetException().ToString());
-            Environment.Exit(1);
-        }
-
-        static void watcher_Created(object sender, FileSystemEventArgs e)
-        {
-            MoveToBackupFolderAndPost(e.FullPath);
-        }
-
-        static void MoveToBackupFolderAndPost(String fullPath)
-        {
-            FileSystemInfo backup;
-            FileAttributes attributes = File.GetAttributes(fullPath);
-            if (attributes.HasFlag(FileAttributes.Directory))
-            {
-                backup = MoveFolderToBackup(fullPath);
-            }
-            else
-            {
-                backup = MoveFileToBackup(fullPath);
-            }
-
-            AddItemToPostingDb(backup);
-        }
-
-        private static FileSystemInfo MoveFolderToBackup(String fullPath)
-        {
-            FileSystemInfo backup;
-            DirectoryInfo toPost = new DirectoryInfo(fullPath);
-            String destinationFolder = Path.Combine(autoPosterconfig.BackupFolder.FullName, toPost.Name);
-            backup = new DirectoryInfo(destinationFolder);
-            if (backup.Exists)
-                backup.Delete();
-            Boolean retry = true;
-            while (retry)
-            {
-                try
-                {
-                    Directory.Move(fullPath, destinationFolder);
-                    retry = false;
-                }
-                catch (IOException ex)
-                {
-                    if (ex.HResult == -2147024891)  //Only handle file locked events.
-                        Thread.Sleep(500);  //Sleep for half a second before retry.
-                    else
-                        throw;
-                }
-            }
-            return backup;
-        }
-
-        private static FileSystemInfo MoveFileToBackup(String fullPath)
-        {
-            FileSystemInfo backup;
-            FileInfo toPost = new FileInfo(fullPath);
-            String destinationFile = Path.Combine(autoPosterconfig.BackupFolder.FullName, toPost.Name);
-            backup = new FileInfo(destinationFile);
-            if (backup.Exists)
-                backup.Delete();
-            Boolean retry = true;
-            while (retry)
-            {
-                try
-                {
-                    File.Move(fullPath, destinationFile);
-                    retry = false;
-                }
-                catch (IOException ex)
-                {
-                    if (ex.HResult == -2147024864)  //Only handle file locked events.
-                        Thread.Sleep(500);  //Sleep for half a second before retry.
-                    else
-                        throw;
-                }
-            }
-            return backup;
-        }
-
-        private static void AddItemToPostingDb(FileSystemInfo toPost)
-        {
-            UploadEntry newUploadentry = new UploadEntry();
-            newUploadentry.CreatedAt = DateTime.UtcNow;
-            newUploadentry.Name = toPost.Name;
-            newUploadentry.RemoveAfterVerify = autoPosterconfig.RemoveAfterVerify;
-            newUploadentry.Cancelled = false;
-            newUploadentry.Size = toPost.Size();
-            DBHandler.Instance.AddNewUploadEntry(newUploadentry);
         }
     }
 }
---
+++ nntpAutoposter/Watcher.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nntpAutoposter
+{
+    class Watcher
+    {
+        private AutoPosterConfig configuration;
+        private FileSystemWatcher watcher;
+
+        public Watcher(AutoPosterConfig configuration)
+        {
+            this.configuration = configuration;
+            watcher = new FileSystemWatcher(configuration.WatchFolder.FullName);
+            watcher.IncludeSubdirectories = false;
+            watcher.Created += watcher_Created;
+            watcher.Error += watcher_Error;
+
+        }
+
+        public void Start()
+        {
+            foreach (FileSystemInfo toPost in autoPosterconfig.WatchFolder.EnumerateFileSystemInfos())
+            {
+                MoveToBackupFolderAndPost(toPost.FullName);
+            }
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Monitoring '{0}' for new files or folders to post.", configuration.WatchFolder.FullName);
+        }
+
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+        }
+
+        static void watcher_Error(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine("The FileSystemWatcher got an error:");
+            Console.WriteLine(e.GetException().ToString());
+            Environment.Exit(1);
+        }
+
+        static void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            MoveToBackupFolderAndPost(e.FullPath);
+        }
+
+        static void MoveToBackupFolderAndPost(String fullPath)
+        {
+            FileSystemInfo backup;
+            FileAttributes attributes = File.GetAttributes(fullPath);
+            if (attributes.HasFlag(FileAttributes.Directory))
+            {
+                backup = MoveFolderToBackup(fullPath);
+            }
+            else
+            {
+                backup = MoveFileToBackup(fullPath);
+            }
+
+            AddItemToPostingDb(backup);
+        }
+
+        private static FileSystemInfo MoveFolderToBackup(String fullPath)
+        {
+            FileSystemInfo backup;
+            DirectoryInfo toPost = new DirectoryInfo(fullPath);
+            String destinationFolder = Path.Combine(autoPosterconfig.BackupFolder.FullName, toPost.Name);
+            backup = new DirectoryInfo(destinationFolder);
+            if (backup.Exists)
+                backup.Delete();
+            Boolean retry = true;
+            while (retry)
+            {
+                try
+                {
+                    Directory.Move(fullPath, destinationFolder);
+                    retry = false;
+                }
+                catch (IOException ex)
+                {
+                    if (ex.HResult == -2147024891)  //Only handle file locked events.
+                        Thread.Sleep(500);  //Sleep for half a second before retry.
+                    else
+                        throw;
+                }
+            }
+            return backup;
+        }
+
+        private static FileSystemInfo MoveFileToBackup(String fullPath)
+        {
+            FileSystemInfo backup;
+            FileInfo toPost = new FileInfo(fullPath);
+            String destinationFile = Path.Combine(autoPosterconfig.BackupFolder.FullName, toPost.Name);
+            backup = new FileInfo(destinationFile);
+            if (backup.Exists)
+                backup.Delete();
+            Boolean retry = true;
+            while (retry)
+            {
+                try
+                {
+                    File.Move(fullPath, destinationFile);
+                    retry = false;
+                }
+                catch (IOException ex)
+                {
+                    if (ex.HResult == -2147024864)  //Only handle file locked events.
+                        Thread.Sleep(500);  //Sleep for half a second before retry.
+                    else
+                        throw;
+                }
+            }
+            return backup;
+        }
+
+        private static void AddItemToPostingDb(FileSystemInfo toPost)
+        {
+            UploadEntry newUploadentry = new UploadEntry();
+            newUploadentry.CreatedAt = DateTime.UtcNow;
+            newUploadentry.Name = toPost.Name;
+            newUploadentry.RemoveAfterVerify = autoPosterconfig.RemoveAfterVerify;
+            newUploadentry.Cancelled = false;
+            newUploadentry.Size = toPost.Size();
+            DBHandler.Instance.AddNewUploadEntry(newUploadentry);
+        }
+    }
+}
