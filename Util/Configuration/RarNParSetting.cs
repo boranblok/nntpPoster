c5aee950bc3d7a910c3f4d2e87dc8cbb71a272e2
boranblok <ben.blok@gmail.com>
2015-09-30 13:48:21 +0200

    preparation to move to a custom unified configuration that stores more intelligent data structures. In later preparation for a simple UI to change these.

---
+++ Util/Configuration/RarNParSetting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Configuration
+{
+    [DataContract(Namespace = "Util.Configuration")]
+    public class RarNParSetting
+    {
+        [DataMember(Order = 0)]
+        public Int32 FromSize { get; set; }
+
+        public Int64 FromSizeBytes
+        {
+            get { return FromSize*1024*1024; }
+        }
+
+        [DataMember(Order = 1)]
+        public Int32 RarSize { get; set; }
+
+        [DataMember(Order = 2)]
+        public Int32 Par2Percentage { get; set; }
+    }
+}
