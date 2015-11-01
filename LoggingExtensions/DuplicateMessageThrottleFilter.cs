eed5ecc6f4744a1796f8adda1a327960a8331efa
boranblok <ben.blok@gmail.com>
2015-11-01 19:58:17 +0100

    added the LoggingExtensions project to git.

---
+++ LoggingExtensions/DuplicateMessageThrottleFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Core;
+using log4net.Filter;
+
+namespace LoggingExtensions
+{
+    class DuplicateMessageThrottleFilter : FilterSkeleton
+    {
+        private String lastMessage;
+
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+        }
+
+        public override FilterDecision Decide(LoggingEvent loggingEvent)
+        {
+            String newMessage = null;
+            if (loggingEvent.MessageObject != null)
+            {
+                newMessage = loggingEvent.MessageObject.ToString();
+            }
+
+            if (newMessage == lastMessage)
+            {
+                return FilterDecision.Deny;
+            }
+
+            lastMessage = newMessage;
+            return FilterDecision.Accept;
+        }
+    }
+}
